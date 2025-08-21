75cf6ffb213d3e068b7f62aaadc9969512506703
Sté[PH]ane <steph.guery@gmail.com>
2025-08-21 07:38:51 +0000

    feat: Ajouter le contrôleur Me et le modèle de réponse utilisateur

---
+++ src/Controller/MeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class MeController extends AbstractController
+{
+    #[Route('/api/me', name: 'app_me', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function me(): JsonResponse
+    {
+        /** @var User|null $user */
+        $user = $this->getUser();
+        if (!$user) {
+            return new JsonResponse(['error' => 'Utilisateur non authentifié'], 401);
+        }
+
+        return new JsonResponse([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
